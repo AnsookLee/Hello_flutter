4bd28493b0ef2a183999b07aaa84199165555acb
AnsookLee <ash51541801@gmail.com>
2026-03-09 13:17:40 +0900

    counter

--- chapter3_stateful_widget/lib/main.dart
+++ chapter3_stateful_widget/lib/main.dart
@@ -14,10 +14,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int counter = 0;
 
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(backgroundColor: Color(0xFFF4EDDB), body: Center()),
+      home: Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Click Count', style: TextStyle(fontSize: 30)),
+              Text('$counter', style: TextStyle(fontSize: 30)),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: Icon(Icons.add_box_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
